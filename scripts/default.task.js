e71295f9148da6524abdf1f9d91e8ed593ba1fcd
Sir0xb <Sir0xb@live.com>
2016-11-10 23:32:06 +0800

    添加表单(未完成)

--- scripts/default.task.js
+++ scripts/default.task.js
@@ -2,6 +2,23 @@
 
 module.exports = function (gulp, $) {
     return function () {
-        console.info("default test");
+        $.rubySass([`${base}/public/css/*.scss`], {
+            sourcemap: true
+        })
+        .pipe($.plumber())
+        .pipe($.base64({
+            maxImageSize: 1024
+        }))
+        .pipe($.minifyCss())
+        .pipe($.sourcemaps.write("maps", {
+            includeContent: false
+        }))
+        .pipe(gulp.dest(`${base}/public/css/`))
+        .on("end", function () {
+            let endTime = new Date();
+            config.jobEnd($, endTime, 'SASS compile', endTime.getTime() - startTime.getTime(), base);
+
+            def.resolve();
+        });
     };
 };
