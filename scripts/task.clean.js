e518252ac483bafe5c9dc1d52276478c27ce3295
Sir0xb <Sir0xb@live.com>
2016-12-01 22:57:19 +0800

    清理脚本增加 iframe 处理

--- scripts/task.clean.js
+++ scripts/task.clean.js
@@ -4,12 +4,19 @@ import cleaner_css       from './cleaner/cleaner.css';
 import cleaner_link      from './cleaner/cleaner.link';
 import cleaner_js        from './cleaner/cleaner.js';
 import cleaner_html      from './cleaner/cleaner.html';
+import cleaner_iframe    from './cleaner/cleaner.iframe';
 
 module.exports = function (gulp, $) {
-    return $.great_promise([
-        cleaner_css(gulp, $),
-        cleaner_link(gulp, $),
-        cleaner_js(gulp, $),
-        cleaner_html(gulp, $)
-    ]);
+    return $.q.fcall(function () {
+        return $.jobStart('Task clean');
+    }).then(function () {
+        return $.great_promise([
+            cleaner_css(gulp, $),
+            cleaner_link(gulp, $),
+            cleaner_js(gulp, $),
+            cleaner_html(gulp, $)
+        ]);
+    }).then(function () {
+        return cleaner_iframe(gulp, $);
+    });
 };
